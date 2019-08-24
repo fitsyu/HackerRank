0f3654f7019f3c74cecdbb74700df6fc7470694f
Fitsyu Two <fitsyu2@gmail.com>
2019-08-24 17:01:34 +0700

    add TwoStrings test

---
+++ HackerRankTests/Hash and Dictionaries/Two Strings Tests.swift
@@ -0,0 +1,39 @@
+//
+//  Two Strings.swift
+//  HackerRankTests
+//
+//  Created by Fitsyu  on 24/08/19.
+//  Copyright © 2019 Fitsyu . All rights reserved.
+//
+
+import XCTest
+@testable import HackerRank
+
+class TwoStringsTests: XCTestCase {
+    
+    func test1() {
+        
+        // given
+        let str1 = "hello"
+        let str2 = "world"
+        
+        // when
+        let haveSharedSubstring = twoStrings(s1: str1, s2: str2)
+        
+        // then
+        XCTAssertEqual(haveSharedSubstring, true) // share 'o'
+    }
+    
+    func test2() {
+        
+        // given
+        let str1 = "hi"
+        let str2 = "world"
+        
+        // when
+        let haveSharedSubstring = twoStrings(s1: str1, s2: str2)
+        
+        // then
+        XCTAssertEqual(haveSharedSubstring, false) // share nothing
+    }
+}
